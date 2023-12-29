d1a3b48c6198f21dc918f2a751d6bbccc218f372
Saimaheswari <124231032+Saimaheswari@users.noreply.github.com>
2023-12-29 10:04:42 +0530

    Update hai.py

--- hai.py
+++ hai.py
@@ -1,4 +1,5 @@
 import streamlit as st
-st.set_page_config(page_titel="heloo")
-st.markdown("##helloooo")
-st.write("##welcome")
+st.write("<h1>Hello world</h1>",unsafe_allow_html=True)
+#ing colour of test
+st.write("<h1ss style='color:red;'>hello world</h1>",unsafe_allow_html=True)
+st.file_uploader("upload file")
