d9f031feb2508c38fae5c54ced3caea21d0a63b2
Saimaheswari <124231032+Saimaheswari@users.noreply.github.com>
2023-12-29 10:25:37 +0530

    Update hai.py

--- hai.py
+++ hai.py
@@ -5,8 +5,8 @@ st.write("<h1 style='color:blue;'>Good morning</h1>",unsafe_allow_html=True)
 
 st.file_uploader("upload file")
 
-st.image("https://img.freepik.com/free-photo/beautiful-rose-nature_23-2150737339.jpg")
-st.video("https://youtu.be/_5IuCKHwpjo")
+st.image("https://animaders.com/wp-content/uploads/2021/10/Tom-and-Jerry-Cartoon-A-Short-History-of-Americas-Favorite-Cartoon-Team-animader.jpg")
+st.video("https://youtu.be/AGBjI0x9VbM?si=M34MEMScBz7EjkKe")
 img = st.file_uploader("Upload image")
 st.image(img)
 
