0b688d4e95c80c4add4376ea2125594084308ad9
Saimaheswari <124231032+Saimaheswari@users.noreply.github.com>
2023-12-29 10:18:28 +0530

    Update hai.py

--- hai.py
+++ hai.py
@@ -6,7 +6,7 @@ st.write("<h1 style='color:blue;'>Good morning</h1>",unsafe_allow_html=True)
 st.file_uploader("upload file")
 
 st.image("https://img.freepik.com/free-photo/beautiful-rose-nature_23-2150737339.jpg")
-st.vedio("https://youtu.be/KGGzd8UxQQ4?si=LzDoaJkhElWEynlc")
+st.video("https://youtu.be/KGGzd8UxQQ4?si=LzDoaJkhElWEynlc")
 img = st.file_uploader("Upload image")
 st.image(img)
 
